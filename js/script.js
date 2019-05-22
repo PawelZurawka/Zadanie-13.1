5c83ed683d634a63e37e6e1bfc1db01cb1862133
Paweł Żurawka <pzurawka@gmail.com>
2019-05-22 22:14:03 +0200

    Task complete

--- js/script.js
+++ js/script.js
@@ -1,40 +1,47 @@
-'use strict'; 
-(function(){ 
+'use strict';
 
 var data = [
-  {
-    id: 'box1',
-    title: 'First box',
-    content: '<p>Lorem ipsum dolor sit amet!</p>',
-    categories: ['highlighted', 'special-header', 'important']
-  },
-  {
-    id: 'box2',
-    title: 'Second box',
-    content: '<p>Lorem ipsum dolor sit amet!</p>',
-    categories: ['special-header', 'important']
-  },
-  {
-    id: 'box3',
-    title: 'Third box',
-    content: '<p>Lorem ipsum dolor sit amet!</p>',
-    categories: ['highlighted', 'important']
-  },
-  {
-    id: 'box4',
-    title: 'Fourth box',
-    content: '<p>Lorem ipsum dolor sit amet!</p>',
-    categories: ['highlighted']
-  },
-  {
-    id: 'box5',
-    title: 'Fifth box',
-    content: '<p>Lorem ipsum dolor sit amet!</p>',
-    categories: []
-  },
+	{
+		id: 'box1',
+		title: 'First box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'special-header', 'important']
+	},
+	{
+		id: 'box2',
+		title: 'Second box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['special-header', 'important']
+	},
+	{
+		id: 'box3',
+		title: 'Third box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'important']
+	},
+	{
+		id: 'box4',
+		title: 'Fourth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted']
+	},
+	{
+		id: 'box5',
+		title: 'Fifth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: []
+	},
 ];
 
-var boxes = function(boxID, boxTitle, boxContent, boxCategories) {
-  
-};
-})();
+for (var i = 0; i < data.length; i++ ) {
+	var box = document.createElement('div');
+	var header = document.createElement('header');
+
+	document.body.appendChild(box);
+	box.id = data[i].id;
+	box.className = data[i].categories.join(' ');
+
+	box.appendChild(header);
+	header.innerHTML = data[i].title;
+	box.innerHTML += data[i].content;
+}
